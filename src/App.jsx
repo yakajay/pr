4f99042a5c1a656e4c8fb4751b8e90b1ea65f622
Yakkali Ajay Kumar <102503364+yakajay@users.noreply.github.com>
2026-02-12 21:01:58 +0530

    basic code change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
 
 const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(flase);
+  const [scrolled, setScrolled] = useState(false);
+  const logoPath = "/yak-logo.png";
+
+  useEffect (() =>  {
+    const handeScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+    window.addEventListener('scroll', handeScroll);
+    return () => window.removeEventListener('scroll', handeScroll);
+  }, []);
+
   return (
-    <div>
-      <Routes/>
+    <div className='min-h-screen bg-white'>
+      <nav className={`fixed w-full z-50 transition-all duration-300 ${scrolled ? 'bg-white shadow-lg' : 'bg-transparent'
+        }`}>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between items-center py-4">
+            <div className="flex items-center">
+              <img src={logoPath} alt='logo' className='h-40 w-auto'/>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Routes } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Routes>
+      <App />
+    </Routes>
   </StrictMode>,
 )
